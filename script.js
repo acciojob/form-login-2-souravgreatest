5a93e6ff209062ef975b213c17292d7d77e3b99f
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-06 13:43:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let form=document.getElementById('form');
+let val=form.eventListener('click',(e)={
+	e.preventDefault();
+	// alert(e);
+	document.getElementById('first-name').value='Varuos';
+	alert({
+		`First Name: John Last Name: Doe Phone Number: 1234567890 Email ID: ${e.target.email}`
+	})
+})
